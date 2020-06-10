8d47fa86220d5e648c184c46a466f5fcd9a1ded2
Briseida Pagador <bpagador@gmail.com>
2020-06-10 16:34:14 -0700

    poll schema with org object

---
+++ __tests__/polls.test.js
@@ -0,0 +1,142 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+const Poll = require('../lib/models/Poll');
+const Organization = require('../lib/models/Organization');
+
+
+describe('poll routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let organization;
+  beforeEach(async() => {
+    organization = await Organization.create({
+      name: 'People Power Party (PPP)',
+      title: 'community organization',
+      description: ['community led, funded, and supported'],
+      imageURL: 'image1.com'
+    });
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+  it('creates a poll via POST', () => {
+    return request(app)
+      .post('/api/v1/polls')
+      .send({
+        organization: organization._id,
+        title: 'Free Medical Services for Grades K-12',
+        description: 'reallocates police funding and applies it to free healthcare for all public school students',
+        option: ['Yes, fully support']
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          organization: organization.id,
+          title: 'Free Medical Services for Grades K-12',
+          description: 'reallocates police funding and applies it to free healthcare for all public school students',
+          option: ['Yes, fully support'],
+          __v: 0
+        });
+      });
+  });
+
+  // it('can get all the polls via GET', () => {
+  //   return Poll.create({
+  //     title: 'Free Medical Services for Grades K-12',
+  //     description: 'reallocates police funding and applies it to free healthcare for all public school students',
+  //     option: ['Yes, fully support']
+  //   })
+  //     .then(() => request(app).get('/api/v1/organizations'))
+  //     .then(res => {
+  //       expect(res.body).toEqual([{
+  //         _id: expect.anything(),
+  //         title: 'Free Medical Services for Grades K-12',
+  //       }]);
+  //     });
+  // });
+
+  // it('gets an organization by id via GET', () => {
+  //   return Organization.create({
+  //     name: 'Black Leaders Alliance',
+  //     title: 'leaders coalition',
+  //     description: ['promotes Black leadership and initiatives'],
+  //     imageURL: 'image3.com'
+  //   })
+  //     .then(organization => request(app).get(`/api/v1/organizations/${organization._id}`))
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.anything(),
+  //         name: 'Black Leaders Alliance',
+  //         title: 'leaders coalition',
+  //         description: ['promotes Black leadership and initiatives'],
+  //         imageURL: 'image3.com',
+  //         __v: 0
+  //       });
+  //     });
+  // });
+
+  // it('updates an organization by its id via PATCH', () => {
+  //   return Organization.create({
+  //     name: 'Environment Law Party',
+  //     title: 'leaders coalition',
+  //     description: ['preserves lands and natural resources'],
+  //     imageURL: 'image4.com'
+  //   })
+
+  //     .then(organization => {
+  //       return request(app)
+  //         .patch(`/api/v1/organizations/${organization._id}`)
+  //         .send({ 
+  //           title: 'lawyers and bill writers', 
+  //           description: ['community led, funded, and supported', 'preserves lands and natural resources'] });
+  //     })
+
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.anything(),
+  //         name: 'Environment Law Party',
+  //         title: 'lawyers and bill writers',
+  //         description: ['community led, funded, and supported', 'preserves lands and natural resources'],
+  //         imageURL: 'image4.com',
+  //         __v: 0
+  //       });
+  //     });
+  // });
+
+  // it('deletes an organization by its id via DELETE', () => {
+  //   return Organization.create({
+  //     name: 'People Power Party (PPP)',
+  //     title: 'community organization',
+  //     description: ['community led, funded, and supported'],
+  //     imageURL: 'image1.com'
+  //   })
+
+  //     .then(organization => request(app).delete(`/api/v1/organizations/${organization._id}`))
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.anything(),
+  //         name: 'People Power Party (PPP)',
+  //         title: 'community organization',
+  //         description: ['community led, funded, and supported'],
+  //         imageURL: 'image1.com',
+  //         __v: 0
+  //       });
+  //     });
+  // });
+});
