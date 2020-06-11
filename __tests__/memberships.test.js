ff15e279644a5586b9957cf0785c85fd5ef5ff4c
Briseida Pagador <bpagador@gmail.com>
2020-06-11 15:03:42 -0700

    all tests pass for membership

--- __tests__/memberships.test.js
+++ __tests__/memberships.test.js
@@ -22,6 +22,8 @@ describe('membership routes', () => {
   });
 
   let organization;
+  let user;
+
   beforeEach(async() => {
     organization = await Organization.create({
       name: 'People Power Party (PPP)',
@@ -29,10 +31,7 @@ describe('membership routes', () => {
       description: ['community led, funded, and supported'],
       imageURL: 'image1.com'
     });
-  });
 
-  let user;
-  beforeEach(async() => {
     user = await User.create({
       name: 'Briseida',
       phone: '111-222-3344',
@@ -41,7 +40,7 @@ describe('membership routes', () => {
       imageURL: 'imageB.com'
     });
   });
-  
+
 
   afterAll(async() => {
     await mongoose.connection.close();
@@ -65,96 +64,49 @@ describe('membership routes', () => {
       });
   });
 
-  it.only('can get all the users in an organization by id via GET', async() => {
-    await Membership.create([{
+  it('can get all the users part of an organization via GET', async() => {
+    await Membership.create({
       organization: organization._id,
       user: user._id
-    }, {
-      organization: organization._id,
-      user: user._id
-    }
-    ]);
-    return request(app).get(`/api/v1/memberships?organization=${organization.id}`)
+    });
+    return request(app).get(`/api/v1/memberships?organization=${organization._id}`)
       .then(res => {
-        expect(res.body).toEqual([{
-          _id: expect.anything
-        }]);
+        expect(res.body).toEqual([{ 
+          '__v': 0, 
+          '_id': expect.anything(), 
+          'organization': { '_id': expect.anything(), 'title': 'community organization', 'imageURL': 'image1.com'  }, 
+          'user': { '_id': expect.anything(), 'name': 'Briseida', 'imageURL': 'imageB.com' } }]
+        );
       });
   });
 
-  it('gets a poll by id via GET', () => {
-    return Poll.create({
+  it('can get all the organizations the user is a part of via GET', async() => {
+    await Membership.create({
       organization: organization._id,
-      title: 'Free Medical Services for Grades K-12',
-      description: 'reallocates police funding and applies it to free healthcare for all public school students',
-      option: ['Yes, fully support'],
-      imageURL: 'image1.com'
-    })
-      .then(poll => request(app).get(`/api/v1/polls/${poll._id}`))
+      user: user._id
+    });
+    return request(app).get(`/api/v1/memberships?user=${user._id}`)
       .then(res => {
-        expect(res.body).toEqual({
-          _id: expect.anything(),
-          organization: {
-            _id: organization.id,
-            name: 'People Power Party (PPP)'
-          },
-          title: 'Free Medical Services for Grades K-12',
-          description: 'reallocates police funding and applies it to free healthcare for all public school students',
-          option: ['Yes, fully support'],
-          imageURL: 'image1.com',
-          __v: 0
-        });
+        expect(res.body).toEqual([{ 
+          '__v': 0, 
+          '_id': expect.anything(), 
+          'organization': { '_id': expect.anything(), 'title': 'community organization', 'imageURL': 'image1.com'  }, 
+          'user': { '_id': expect.anything(), 'name': 'Briseida', 'imageURL': 'imageB.com' } }]
+        );
       });
   });
 
-  it('updates a poll by its id via PATCH', () => {
-    return Poll.create({
+  it('deletes a membership DELETE', () => {
+    return Membership.create({
       organization: organization._id,
-      title: 'Free Medical Services for Grades K-12',
-      description: 'reallocates police funding and applies it to free healthcare for all public school students',
-      option: ['Yes, fully support'],
-      imageURL: 'image1.com'
+      user: user._id
     })
-
-      .then(poll => {
-        return request(app)
-          .patch(`/api/v1/polls/${poll._id}`)
-          .send({ 
-            title: 'Mental Health Subsidies in the Workplace', 
-            description: '$500 subsidy towards mental health for every uninsured ' });
-      })
-
+      .then(member => request(app).delete(`/api/v1/memberships/${member.id}`))
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
           organization: organization.id,
-          title: 'Mental Health Subsidies in the Workplace', 
-          description: '$500 subsidy towards mental health for every uninsured ',
-          option: ['Yes, fully support'],
-          imageURL: 'image1.com',
-          __v: 0
-        });
-      });
-  });
-
-  it('deletes a poll by its id via DELETE', () => {
-    return Poll.create({
-      organization: organization._id,
-      title: 'Free Medical Services for Grades K-12',
-      description: 'reallocates police funding and applies it to free healthcare for all public school students',
-      option: ['Yes, fully support'],
-      imageURL: 'image1.com'
-    })
-
-      .then(poll => request(app).delete(`/api/v1/polls/${poll._id}`))
-      .then(res => {
-        expect(res.body).toEqual({
-          _id: expect.anything(),
-          organization: organization.id,
-          title: 'Free Medical Services for Grades K-12',
-          description: 'reallocates police funding and applies it to free healthcare for all public school students',
-          option: ['Yes, fully support'],
-          imageURL: 'image1.com',
+          user: user.id,
           __v: 0
         });
       });
