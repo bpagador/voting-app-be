8f8ce997ba4f3b8481e63f8e3c51a6389910b487
Briseida Pagador <bpagador@gmail.com>
2020-06-11 16:10:42 -0700

    creates votes via post test is working

--- __tests__/votes.test.js
+++ __tests__/votes.test.js
@@ -9,9 +9,10 @@ const app = require('../lib/app');
 const User = require('../lib/models/User');
 const Organization = require('../lib/models/Organization');
 const Membership = require('../lib/models/Membership');
+const Poll = require('../lib/models/Poll');
 
 
-describe('membership routes', () => {
+describe('vote routes', () => {
   beforeAll(async() => {
     const uri = await mongod.getUri();
     return connect(uri);
@@ -23,6 +24,7 @@ describe('membership routes', () => {
 
   let organization;
   let user;
+  let poll;
 
   beforeEach(async() => {
     organization = await Organization.create({
@@ -39,76 +41,86 @@ describe('membership routes', () => {
       communicationMedium: ['phone'],
       imageURL: 'imageB.com'
     });
-  });
 
+    poll = await Poll.create({
+      organization: organization._id,
+      title: 'Free Medical Services for Grades K-12',
+      description: 'reallocates police funding and applies it to free healthcare for all public school students',
+      option: ['Yes', 'No', 'Undecided'],
+      imageURL: 'image1.com'
+    });
+
+  });
 
   afterAll(async() => {
     await mongoose.connection.close();
     return mongod.stop();
   });
 
-  it('creates a membership via POST', () => {
+  it('creates a vote via POST', () => {
     return request(app)
-      .post('/api/v1/memberships')
+      .post('/api/v1/votes')
       .send({
-        organization: organization._id,
-        user: user._id
+        poll: poll._id,
+        user: user._id,
+        option: 'Yes'
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
-          organization: organization.id,
+          poll: poll.id,
           user: user.id,
+          option: 'Yes',
           __v: 0
         });
       });
   });
 
-  it('can get all the users part of an organization via GET', async() => {
-    await Membership.create({
-      organization: organization._id,
-      user: user._id
-    });
-    return request(app).get(`/api/v1/memberships?organization=${organization._id}`)
-      .then(res => {
-        expect(res.body).toEqual([{ 
-          '__v': 0, 
-          '_id': expect.anything(), 
-          'organization': { '_id': expect.anything(), 'title': 'community organization', 'imageURL': 'image1.com'  }, 
-          'user': { '_id': expect.anything(), 'name': 'Briseida', 'imageURL': 'imageB.com' } }]
-        );
-      });
-  });
+  // it('can get all the users part of an organization via GET', async() => {
+  //   await Membership.create({
+  //     organization: organization._id,
+  //     user: user._id
+  //   });
+  //   return request(app).get(`/api/v1/memberships?organization=${organization._id}`)
+  //     .then(res => {
+  //       expect(res.body).toEqual([{ 
+  //         '__v': 0, 
+  //         '_id': expect.anything(), 
+  //         'organization': { '_id': expect.anything(), 'title': 'community organization', 'imageURL': 'image1.com'  }, 
+  //         'user': { '_id': expect.anything(), 'name': 'Briseida', 'imageURL': 'imageB.com' } }]
+  //       );
+  //     });
+  // });
 
-  it('can get all the organizations the user is a part of via GET', async() => {
-    await Membership.create({
-      organization: organization._id,
-      user: user._id
-    });
-    return request(app).get(`/api/v1/memberships?user=${user._id}`)
-      .then(res => {
-        expect(res.body).toEqual([{ 
-          '__v': 0, 
-          '_id': expect.anything(), 
-          'organization': { '_id': expect.anything(), 'title': 'community organization', 'imageURL': 'image1.com'  }, 
-          'user': { '_id': expect.anything(), 'name': 'Briseida', 'imageURL': 'imageB.com' } }]
-        );
-      });
-  });
+  // it('can get all the organizations the user is a part of via GET', async() => {
+  //   await Membership.create({
+  //     organization: organization._id,
+  //     user: user._id
+  //   });
+  //   return request(app).get(`/api/v1/memberships?user=${user._id}`)
+  //     .then(res => {
+  //       expect(res.body).toEqual([{ 
+  //         '__v': 0, 
+  //         '_id': expect.anything(), 
+  //         'organization': { '_id': expect.anything(), 'title': 'community organization', 'imageURL': 'image1.com'  }, 
+  //         'user': { '_id': expect.anything(), 'name': 'Briseida', 'imageURL': 'imageB.com' } }]
+  //       );
+  //     });
+  // });
 
-  it('deletes a membership DELETE', () => {
-    return Membership.create({
-      organization: organization._id,
-      user: user._id
-    })
-      .then(member => request(app).delete(`/api/v1/memberships/${member.id}`))
-      .then(res => {
-        expect(res.body).toEqual({
-          _id: expect.anything(),
-          organization: organization.id,
-          user: user.id,
-          __v: 0
-        });
-      });
-  });
+  // it('deletes a membership DELETE', () => {
+  //   return Membership.create({
+  //     organization: organization._id,
+  //     user: user._id
+  //   })
+  //     .then(member => request(app).delete(`/api/v1/memberships/${member.id}`))
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         _id: expect.anything(),
+  //         organization: organization.id,
+  //         user: user.id,
+  //         __v: 0
+  //       });
+  //     });
+  // });
 });
